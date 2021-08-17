0e68b32fa3c10ab7460483ecf2d4d3e2db067edf
chalonhutson <chalonhutson@gmail.com>
2021-08-17 09:35:51 -0600

    Git ignored node modules.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "/public/index.html"))});
+
+const port = process.env.PORT || 4040;
+
+
+app.listen(port, () => console.log(`Up and running port ${port}.`))
