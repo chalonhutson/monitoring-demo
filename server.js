16e0543b8b1443b5298b6cb84a32e154a67cac9c
chalonhutson <chalonhutson@gmail.com>
2021-08-17 09:52:42 -0600

    Added rollbar stuff.

--- server.js
+++ server.js
@@ -4,11 +4,29 @@ const app = express();
 
 const path = require("path");
 
+
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'e4fd9bf3d74445b686dab0f0c761a984',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+
+
+
+
 app.use(express.json());
 
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "/public/index.html"))});
+app.get("/", (req, res) => {
+    rollbar.log('Hellors world!')
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 
 const port = process.env.PORT || 4040;
 
 
-app.listen(port, () => console.log(`Up and running port ${port}.`))
+app.listen(port, () => {
+    rollbar.log(`Server running port ${port}.`)
+    console.log(`Up and running port ${port}.`)
+});
